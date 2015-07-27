adfe2570251290ee05566d0eb150d3f475515262
Gordon Wells <gordon.wells@gmail.com>
2015-07-27 23:19:48 +0200

    moved file checking out to seperate function

--- linter.py
+++ linter.py
@@ -21,7 +21,7 @@ class FileExists(Linter):
     """Provides an interface to fileExists."""
 
     # syntax = ('pbs','source.pbs')
-    syntax = ('pbs','source.pbs','shell-unix-generic')
+    syntax = ('pbs', 'source.pbs', 'shell-unix-generic')
     cmd = None
     regex = (
         r'^.+?:(?P<line>\d+):(?P<col>\d+):'
@@ -40,20 +40,46 @@ class FileExists(Linter):
     inline_overrides = None
     comment_re = None
 
-    def linearToRowCol(self,pos,code,tablength=4):
-    	row = 1
-    	currentlen = 0
-    	lastlen = 0
-    	splitcode = code.split('\n')
+    @classmethod
+    def posToRowCol(cls, pos, code):
+        """
+        Convert position in string to rectangular coordinates
+        """
+        row = 1
+        currentlen = 0
+        lastlen = 0
+        splitcode = code.split('\n')
 
-    	for line in splitcode:
-    		lastlen = currentlen
-    		currentlen += len(line)+1
-    		if currentlen >= pos:
-    			return((row,pos-lastlen+1))
-    		row += 1
+        for line in splitcode:
+            lastlen = currentlen
+            currentlen += len(line)+1
+            if currentlen >= pos:
+                return (row, pos-lastlen+1)
+            row += 1
 
-    def scanUnflagged(self,prog,ext,code,input=True):
+    def checkForFile(self, code, path, filename_instance, prog_instance, inputfile=True):
+        filename = filename_instance.group('fname')
+        filenameStart = filename_instance.start('fname')
+        pos = self.posToRowCol(prog_instance.start(0)+filenameStart, code)
+
+        if os.path.isfile(path+"/"+filename):
+            if inputfile:
+                linted = 'W:%s:%s:warning:File exists (%s)\n'%(pos[0], pos[1], filename)
+            else:
+                linted = 'E:%s:%s:error:File exists, will be overwritten (%s)\n'\
+                %(pos[0], pos[1], filename)
+        else:
+            if inputfile:
+                linted = 'E:%s:%s:error:File not found (%s)\n'%(pos[0], pos[1], filename)
+
+        return linted
+
+    def scanUnflagged(self, prog, ext, code, inputfile=True):
+        """
+            Scan for file arguments not preceded by a -/-- type flag.
+            Check if each file exists and return appropriate warning
+            or error messages for each file
+        """
 
         path = os.path.dirname(self.view.file_name())
         all_lints = ''
@@ -61,36 +87,31 @@ class FileExists(Linter):
 
         regex = re.compile(
             r'%s(.+\\\n)*'
-            r'.+\s+([\w\._-]+%s)+\s+.*\n' %(prog,ext)
-            ,re.M)
+            r'.+\s+([\w\._-]+%s)+\s+.*\n' %(prog, ext)
+            , re.M)
 
         for prog_instance in regex.finditer(code):
             # print (prog_instance.group(0))
-            file_regex = re.compile (r'(?P<preceding>[\w_\.-]+)\s+(?P<fname>[\w_\.-]+%s)'%ext)
-          
+            file_regex = re.compile(r'(?P<preceding>[\w_\.-]+)\s+(?P<fname>[\w_\.-]+%s)'%ext)
+
             for file_instance in file_regex.finditer(prog_instance.group(0)):
                 filename = file_instance.group('fname')
                 isflag = re.search('-{1,2}\w+',file_instance.group('preceding'))
                 if not isflag:
-                    filenameStart = file_instance.start('fname')
-                    pos = self.linearToRowCol(prog_instance.start(0)+filenameStart, code)
-
-                    if (os.path.isfile(path+"/"+filename)):
-                        if input:
-                            linted = 'W:%s:%s:warning:File exists (%s)'%(pos[0],pos[1],filename) 
-                        else: 
-                            linted = 'E:%s:%s:error:File exists, will be overwritten (%s)'%(pos[0],pos[1],filename) 
-                    else: 
-                        if input:
-                            linted = 'E:%s:%s:error:File not found (%s)'%(pos[0],pos[1],filename) 
-
-                    if linted:
-                        all_lints += '\n'+linted
+                    print ("*****"+filename)
+                    linted = self.checkForFile(code, path, file_instance, prog_instance, inputfile)
+                    all_lints += linted
+                
 
         return all_lints
 
  
-    def scanFlagged(self,prog,arg,code,input=True):
+    def scanFlagged(self, prog, arg, code, inputfile=True):
+        """
+            Scan for file arguments preceded by -/-- type flags. 
+            Check if each file exists and return appropriate warning
+            or error messages for each file
+        """
 
         regex = re.compile(
             r'%s(.+\\\n)*'
@@ -101,52 +122,43 @@ class FileExists(Linter):
         linted = None
         path = os.path.dirname(self.view.file_name())
 
-        for prog_instance in regex.finditer(code):        
+        for prog_instance in regex.finditer(code):
             file_regex = re.compile(r'(?P<flag>%s)\s+(?P<fname>[\w\._-]+)'%arg)
 
             for file_instance in file_regex.finditer(prog_instance.group(0)):
-                filenameStart = file_instance.start('fname')
-                filename = file_instance.group('fname')
-                pos = self.linearToRowCol(prog_instance.start(0)+filenameStart,code)
-
-                if (os.path.isfile(path+"/"+filename)):
-                    if input:
-                        linted = 'W:%s:%s:warning:File exists (%s)'%(pos[0],pos[1],filename) 
-                    else: 
-                        linted = 'E:%s:%s:error:File exists, will be overwritten (%s)'%(pos[0],pos[1],filename) 
-                else: 
-                    if input:
-                        linted = 'E:%s:%s:error:File not found (%s)'%(pos[0],pos[1],filename) 
-
-                if linted:
-                    all_lints += '\n'+linted
+                linted = self.checkForFile(code, path, file_instance, prog_instance, inputfile)
+                all_lints += linted
 
         return all_lints
 
 
-    def read_fileArgs(self, scope):
+    def readFileArgs(self, scope):
+        """
+        Read predefined program name and flag assocations from all
+        *.fileArg files is sublime 3 config folder.
+        """
         flagFiles = sublime.find_resources("*.fileArgs")       
 
         for flaglist in flagFiles:
-            print (flaglist)
-            flagdata = json.loads(sublime.load_resource(flaglist) )
+            print(flaglist)
+            flagdata = json.loads(sublime.load_resource(flaglist))
 
             if flagdata['scope'] in scope:
-                print (flagdata['progname'])
+                print(flagdata['progname'])
                 return flagdata
 
         return False
 
 
-    def run(self,cmd,code):
-        # untabbed = code.replace('\t',' '*4)
-        # untabbed = code
-        splitcode = code.split('\n')
-        found_schrod = False
+    def run(self, cmd, code):
+        """
+        Return error and warning messages internally instead of
+        running through external linter
+        """
 
         scope_name = self.view.scope_name(0)
         print(scope_name)
-        fromFile = self.read_fileArgs(scope_name)
+        fromFile = self.readFileArgs(scope_name)
 
         all_lints = ''
 
@@ -159,7 +171,7 @@ class FileExists(Linter):
         for ext in fromFile['unflaggedExts']:
             all_lints += self.scanUnflagged(fromFile['progname'], ext, code)
 
-        print (all_lints)
+        print(all_lints)
 
-        return(all_lints)
+        return all_lints
 
